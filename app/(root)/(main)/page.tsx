7a3876d328ecccf16facb1dde501410d1e9e33ec
Cameron Mckeel <117554357+xXEpicurusXx@users.noreply.github.com>
2024-04-12 16:09:01 +0900

    still working on initial DB connections

--- app/(root)/(main)/page.tsx
+++ app/(root)/(main)/page.tsx
@@ -8,6 +8,11 @@ export default function Home() {
         afterSignOutUrl="/"
       />
       <ModeToggle/>
+      <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100">
+
+
+      </div>
+
     </main>
   );
 }
