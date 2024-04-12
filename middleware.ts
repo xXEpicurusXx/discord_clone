e19955a69fa8f508fddfb3d3bb680941890bb02b
Cameron Mckeel <117554357+xXEpicurusXx@users.noreply.github.com>
2024-04-12 11:57:37 +0900

    .

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,11 @@
 import { authMiddleware } from "@clerk/nextjs";
- 
+
 export default authMiddleware({
-    publicRoutes: ["api/webhook/clerk",],
+  publicRoutes: ["api/webhook/clerk"],
 
-    ignoredRoutes: ["/api/webhook/clerk"],
-
+  ignoredRoutes: ["/api/webhook/clerk"],
 });
- 
+
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
