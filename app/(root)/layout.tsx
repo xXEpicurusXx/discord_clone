7a4b0f147a879e8cd0a5ba6ae09d03970a45073d
Cameron Mckeel <117554357+xXEpicurusXx@users.noreply.github.com>
2024-04-12 13:05:27 +0900

    .

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+
+import "../globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+
+const font = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "T.A.I.O.P",
+  description:
+    "A Next.js 14 Meta All In One Place social application for those visition the local Tokyo Area. ",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <body className={font.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem={false}
+            storageKey="discord-theme"
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,11 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <main className="">
-      <p className="text-3xl font-bold text-indigo-500">Discord Clone</p> 
-      <Button>Click Me</Button>
+    <main>
+      <UserButton
+        afterSignOutUrl="/"
+      />
     </main>
   );
 }
